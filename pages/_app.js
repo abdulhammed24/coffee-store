cf88d40b311948b5151a3fa545dc76cf373f6961
abdulhammed24 <abdlhamd3@gmail.com>
2023-06-05 07:41:33 +0100

    all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// import StoreProvider from "../store/store-context";
+import { IBM_Plex_Sans } from "next/font/google";
 
+const ibms = IBM_Plex_Sans({ weight: "400", style: "normal", subsets: ["latin"] });
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={ibms.className}>
+      {/* <div> */}
+      {/* <StoreProvider> */}
+      <Component {...pageProps} />
+      {/* </StoreProvider> */}
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
